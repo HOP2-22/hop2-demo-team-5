ba088e91c566b93da21830e40f5a2dc617be129f
Bilguun <Bilguun@nest-34s-iMac.local>
2023-03-29 16:51:02 +0800

    project started

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import Header from "@/layout/header";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
 }
