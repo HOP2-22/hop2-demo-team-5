62c82ab7dc6444748af467494dbf73302ab3a358
Huuji <huuji@Fulxe-4.local>
2023-04-14 19:39:05 +0800

    push

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,21 +1,48 @@
-import { Box, Typography } from "@mui/material";
-
+import { Box, Typography, Stack } from "@mui/material";
+import StartIcon from "@mui/icons-material/Start";
+import KeyboardReturnIcon from "@mui/icons-material/KeyboardReturn";
+import ImportExportIcon from "@mui/icons-material/ImportExport";
+import { Card } from "./card";
 export const SideBar = () => {
   return (
     <Box
       sx={{
-        width: {
-          xl: "20vw",
-          lg: "15vw",
-          md: "50px",
-          sm: "50px",
-          xs: "50px",
-        },
-        height: "90vh",
-        backgroundColor: "white",
+        height: "100%",
+        backgroundColor: "rgb(31,31,35)",
+        color: "white",
+        padding: 2,
+        width: "240px",
       }}
     >
-      <Typography>hello</Typography>
+      <Stack spacing={1}>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Typography sx={{ fontSize: "18px" }}>For You</Typography>
+          <KeyboardReturnIcon sx={{ fontSize: "21px" }} />
+        </Box>
+
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Typography sx={{ fontSize: "13px", fontWeight: "bold" }}>
+            FOLLOWED CHANNELS
+          </Typography>
+          <ImportExportIcon sx={{ fontSize: "21px" }} />
+        </Box>
+        <Stack spacing={1}>
+          <Card />
+          <Card />
+        </Stack>
+      </Stack>
+
+      <Box sx={{ mt: 5 }}>
+        <Typography sx={{ fontSize: "12px", fontWeight: "bold" }}>
+          RECOMMENDED CHANNELS
+        </Typography>
+      </Box>
+
+      <Stack sx={{ mt: 1 }}>
+        <Card />
+        <Card />
+        <Card />
+      </Stack>
     </Box>
   );
 };
